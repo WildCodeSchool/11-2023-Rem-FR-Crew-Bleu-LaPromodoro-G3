b3e42f77582b90494cd48d6c71431523a3739d0f
Ioana Horeanu <ioana.catalina10@yahoo.com>
2024-01-11 11:42:48 +0100

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,26 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import GitPage from "./components/GitPage";
+import Linkedin from "./components/Linkedin";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/git",
+        element: <GitPage />,
+      },
+      {
+        path: "/linkedin",
+        element: <Linkedin />,
+      },
+    ],
   },
 ]);
 
