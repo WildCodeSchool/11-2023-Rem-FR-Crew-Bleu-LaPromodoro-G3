7071b84887e625f2188cf6ff6c5b1e452d22ee34
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2024-01-11 15:34:46 +0100

    Homepage commented (feature in stand-by)

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,45 +1,45 @@
-import React, { useState } from "react";
-import Modal from "./Modal";
-import { images } from "../images/images";
-import avatarDefaut from "../images/avatarDefaut.jpg";
+// import React, { useState } from "react";
+// import Modal from "./Modal";
+// import { images } from "../images/images";
+// import avatarDefaut from "../images/avatarDefaut.jpg";
 
-function HomePage() {
-  const [showModal, setShowModal] = useState(false);
-  const [avatarProfile, setAvatarProfile] = useState(avatarDefaut);
-  const [user, setUser] = useState("invité");
-  const [selectedTheme, setSelectedTheme] = useState(null);
+// function HomePage() {
+//   const [showModal, setShowModal] = useState(false);
+//   const [avatarProfile, setAvatarProfile] = useState(avatarDefaut);
+//   const [user, setUser] = useState("invité");
+//   const [selectedTheme, setSelectedTheme] = useState(null);
 
-  const changeAvatarProfile = (index) => {
-    setAvatarProfile(images[index]);
-  };
-  const updateUserInformation = (newPseudo) => {
-    setUser(newPseudo);
-  };
+//   const changeAvatarProfile = (index) => {
+//     setAvatarProfile(images[index]);
+//   };
+//   const updateUserInformation = (newPseudo) => {
+//     setUser(newPseudo);
+//   };
 
-  return (
-    <div>
-      <div
-        className="Profile-user"
-        style={{ backgroundImage: `url(${selectedTheme})` }}
-      >
-        <img
-          src={avatarProfile}
-          alt="Profile"
-          onClick={() => setShowModal(true)}
-          aria-hidden="true"
-        />
-        <label htmlFor="PseudoInput">{user} </label>
-      </div>
-      <Modal
-        showModal={showModal}
-        setShowModal={setShowModal}
-        changeAvatarProfile={changeAvatarProfile}
-        updateUserInformation={updateUserInformation}
-        setSelectedTheme={setSelectedTheme}
-        user={user}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <div
+//         className="Profile-user"
+//         style={{ backgroundImage: `url(${selectedTheme})` }}
+//       >
+//         <img
+//           src={avatarProfile}
+//           alt="Profile"
+//           onClick={() => setShowModal(true)}
+//           aria-hidden="true"
+//         />
+//         <label htmlFor="PseudoInput">{user} </label>
+//       </div>
+//       <Modal
+//         showModal={showModal}
+//         setShowModal={setShowModal}
+//         changeAvatarProfile={changeAvatarProfile}
+//         updateUserInformation={updateUserInformation}
+//         setSelectedTheme={setSelectedTheme}
+//         user={user}
+//       />
+//     </div>
+//   );
+// }
 
-export default HomePage;
+// export default HomePage;
