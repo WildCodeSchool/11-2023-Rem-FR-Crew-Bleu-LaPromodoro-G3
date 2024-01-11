00cbf8cd6b7a32af648b5fd162bcaa7a9be275a9
Romain <romainbigalion@hotmail.fr>
2024-01-11 10:03:53 +0100

    first commit

---
+++ frontend/src/images/theme.jsx
@@ -0,0 +1,9 @@
+import theme1 from "./theme-game-1.png";
+import theme2 from "./theme-game-2.png";
+import theme3 from "./theme-game-3.png";
+import theme4 from "./theme-game-4.png";
+import theme5 from "./theme-game-5.png";
+import theme6 from "./theme-game-6.png";
+
+export const themes = [theme1, theme2, theme3, theme4, theme5, theme6];
+export default themes;
