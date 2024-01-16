5f52dc05b02122eebfccf330fc4a15acd12f599f
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2024-01-16 15:10:26 +0100

    first version of visual for quiz response options added; button for next question added; presenter and speech container adjusted for laptop screen and large desktop screen

---
+++ frontend/src/components/ButtonNext.jsx
@@ -0,0 +1,13 @@
+import "../styles/ButtonNext.css";
+
+function ButtonNext() {
+  return (
+    <div id="buttonNext">
+      <button id="buttonNextQuestion" type="button">
+        Question suivante
+      </button>
+    </div>
+  );
+}
+
+export default ButtonNext;
--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,8 +1,19 @@
+// import { useState, useEffect } from "react";
 import Presentateur from "../components/Presentateur";
 import Navbar2 from "../components/Navbar2";
+import QuizOptions from "../components/QuizOptions";
+import ButtonNext from "../components/ButtonNext";
 import "../styles/Quiz.css";
 
 function Quiz() {
+  // const [quizData, setQuizData] = useState("Welcome to Quiz");
+
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/category/easy")
+  //     .then((res) => res.json())
+  //     .then((data) => setQuizData(data));
+  // }, []);
+
   return (
     <div>
       <Navbar2 />
@@ -11,6 +22,8 @@ function Quiz() {
         idContainer="quizPresPosition"
         idSpeech="quizSpeechPosition"
       />
+      <QuizOptions />
+      <ButtonNext />
     </div>
   );
 }
