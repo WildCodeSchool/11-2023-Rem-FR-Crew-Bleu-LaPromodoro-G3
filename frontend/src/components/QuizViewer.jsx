5d9e32639abf06e891bebaede91b09134790178a
Romain <romainbigalion@hotmail.fr>
2024-01-25 22:07:23 +0100

    modification création quizz pour affichage instantané

--- frontend/src/components/QuizViewer.jsx
+++ frontend/src/components/QuizViewer.jsx
@@ -1,66 +1,33 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-array-index-key */
+import React from "react";
 import "../styles/QuizViewer.css";
 
-function QuizViewer() {
-  const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:4748/quiz/question-and-answers")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("La réponse du réseau n'était pas valide");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setQuestionsAndAnswers(data);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <p>Chargement en cours...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
+function QuizViewer({ questions }) {
   return (
     <div>
-      <div className="inventoryCreation">
-        {/* <h2 id="questionResponse">
-          Questions et réponses enregistrées avec succès :
-        </h2> */}
-      </div>
+      <div className="inventoryCreation" />
       <div className="viewListing">
-        {questionsAndAnswers && questionsAndAnswers.length > 0 && (
+        {questions && questions.length > 0 ? (
           <ul id="listingQuestion">
-            {questionsAndAnswers.map((questionsArray, index) => (
-              // eslint-disable-next-line react/no-array-index-key
+            {questions.map((question, index) => (
               <div key={index}>
-                {questionsArray.map((question, questionIndex) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <div key={questionIndex}>
-                    <h4>Question: {question.question}</h4>
-                    <ul>
-                      {question.options.map((option, optionIndex) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <li key={optionIndex}>
-                          Réponse: {option}
-                          {question.correct_option === optionIndex && (
-                            <span style={{ color: "green" }}>(Correcte)</span>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
+                <h4>Question: {question.question}</h4>
+                <ul>
+                  {question.options.map((option, optionIndex) => (
+                    <li key={optionIndex}>
+                      Réponse: {option.text}
+                      {option.correct_option && (
+                        <span style={{ color: "green" }}>(Correcte)</span>
+                      )}
+                    </li>
+                  ))}
+                </ul>
               </div>
             ))}
           </ul>
+        ) : (
+          <p>Aucune question n'a été ajoutée.</p>
         )}
       </div>
     </div>
